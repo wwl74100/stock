20da3a7b804726f56d90d4b0593d8abf2fec48e0
wwl <wwl74100@163.com>
2026-01-28 13:22:23 +0800

    Update marketCache.java

--- examples/win-futures/src/main/java/com/binance/win/marketCache.java
+++ examples/win-futures/src/main/java/com/binance/win/marketCache.java
@@ -118,7 +118,7 @@ public class marketCache {
                 continue;
             }
             Side recentlySide = recentlySide(symbol, endTime);
-            if (recentlySide != null) {
+            if (side.equals(recentlySide)) {
                 continue;
             }
 
@@ -402,11 +402,11 @@ public class marketCache {
         }
         BigDecimal sell = base.subtract(buy);
         if (buy.compareTo(sell) > 0) {
-            if (buy.divide(sell, 2, RoundingMode.DOWN).compareTo(new BigDecimal(1.5)) > 0) {
+            if (buy.divide(sell, 2, RoundingMode.DOWN).compareTo(new BigDecimal(1.85)) > 0) {
                 result = Side.BUY;
             }
         } else {
-            if (sell.divide(buy, 2, RoundingMode.DOWN).compareTo(new BigDecimal(1.5)) > 0) {
+            if (sell.divide(buy, 2, RoundingMode.DOWN).compareTo(new BigDecimal(1.85)) > 0) {
                 result = Side.SELL;
             }
         }
